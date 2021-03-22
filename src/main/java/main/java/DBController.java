ca1ac9452c9a811ff18bcce4f92110ba7a557380
Erik Peralta Løvaas <erikpl@protonmail.com>
2021-03-22 18:03:15 +0100

    Merged DBController and DBConnection and changed package structure

--- src/main/java/main/java/DBController.java
+++ src/main/java/main/java/DBController.java
@@ -2,12 +2,35 @@ package main.java;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
 
-public class DBController extends DBConnection {
+public class DBController {
+    private Connection connection;
     private String current_user_email;
     private String current_course_id;
     // Update this attribute when either email or course_id change
     private boolean isInstructor;
+
+    // Empty constructor
+    public DBController() {
+    }
+
+    // Method for connecting to the MySQL server
+    public void connect() {
+        try {
+            // Properties object to store DB user credentials
+            Properties credentials = new Properties();
+            credentials.put("user", "root");
+            credentials.put("password", "12345678");
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/piazzini?allowPublicKeyRetrieval=true&autoReconnect=true&useSSL=false",credentials);
+        }
+        // If unable to connect to the MySQL server
+        catch (Exception e) {
+            throw new RuntimeException("Unable to connect", e);
+        }
+    }
 
     // Return -1 for exception
     // Return 0 for successful login
