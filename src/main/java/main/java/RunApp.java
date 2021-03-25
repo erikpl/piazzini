8551ac08f1c08940fb5ddaaf83f5a20fc55070ff
Erik Peralta Løvaas <erikpl@protonmail.com>
2021-03-25 13:35:18 +0100

    Created RunApp

--- src/main/java/main/java/RunApp.java
+++ src/main/java/main/java/RunApp.java
@@ -1,2 +1,14 @@
-package main.java;public class RunApp {
+package main.java;
+
+// Runs the application
+public class RunApp {
+    public static void main(String... args) {
+        int state = 0;
+        CommandLineInterface cli = new CommandLineInterface();
+
+        // Run until the user exits the application
+        while (state != 1) {
+            state = cli.selectUseCase();
+        }
+    }
 }
